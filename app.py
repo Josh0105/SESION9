f97168852fbf74ca4fb31fd17e31168f1105b15f
Jonathan Calo <73148040+Josh0105@users.noreply.github.com>
2020-10-19 22:42:17 -0600

    se ha actualizado

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>Esta pagina está desde el repositorio</h1>"
+    return "<h1>Esta pagina está desde el repositorio 2</h1>"
 
 
 if __name__ == "__main__":
